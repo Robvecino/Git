6e0ea12a79bd424305ea4cf6f7faa7983d8164c9
Roberto Vecino Alonso <robvecino@gmail.com>
2021-07-07 12:17:44 +0200

    OOP 07/07

---
+++ Sem7_8.py
@@ -0,0 +1,10 @@
+
+#! Ejercicios martes
+
+#* POO
+
+municipio_1 = {'mun_id' : '222245', 'population' : 254325, 'den_km2' : 4.44, 'area' : 143215}
+
+municipio_2 = {'mun_id' : '222666', 'population' : 259435, 'den_km2' : 4.94, 'area' : 164315}
+
+#TODO Un objeto es como un diccionario, en el que podemos añadir funciones para que realicen acciones dentro del objeto (funcionalidades).
