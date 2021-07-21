749e10d953ac211705c68f60da1187f751dae680
Roberto Vecino Alonso <85903780+Robvecino@users.noreply.github.com>
2021-07-21 22:08:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 import funcs
 
 count = 0
@@ -8,15 +9,19 @@ while count < 1:
     user = input('Choose an option: ')
     if user == '1':
         try:
-            json = funcs.open_json()
+            data = funcs.open_json()
             new_user = funcs.new_user()
-            for user in json:
+            for user in data:
                 if new_user['name'] == user['name']:
                     new_user['name'] = input('This name has already been used. Please, select another: ')
             funcs.append_json(new_user)
-        except:
+        except JSONDecodeError:
             new_user = funcs.new_user()
             funcs.write_json(new_user)
+        count = funcs.submenu_end(count)
+    elif user == '2':
+        data = funcs.open_json()
+        funcs.login(data)
         count = funcs.submenu_end(count)
     elif user == '3':
         count += 1
