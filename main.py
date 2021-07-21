3449d97e77f91068f831086d4b1626186894cdec
Roberto Vecino Alonso <85903780+Robvecino@users.noreply.github.com>
2021-07-21 12:54:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import funcs
 
 count = 0
 
-database = []
-
 while count < 1:
     funcs.menu()
     user = input('Choose an option: ')
     if user == '1':
         try:
             json = funcs.open_json()
-            new_user = funcs.new_user(database)
-            json.append(new_user)
+            new_user = funcs.new_user()
+            for user in json:
+                if new_user['name'] == user['name']:
+                    new_user['name'] = input('This name has already been used. Please, select another: ')
+            funcs.append_json(new_user)
         except:
-            new_user = funcs.new_user(database)
+            new_user = funcs.new_user()
             funcs.write_json(new_user)
+        count = funcs.submenu_end(count)
+    elif user == '3':
+        count += 1
