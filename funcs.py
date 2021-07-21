6750a9e52fc186db4830084c5758405fbeeee61f
Roberto Vecino Alonso <85903780+Robvecino@users.noreply.github.com>
2021-07-21 12:54:44 +0200

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -6,26 +6,44 @@ CWD = os.path.dirname(__file__)
 def menu():
     print('1. Create user')
     print('2. Login')
+    print('3. Exit')
 
-
-def new_user(db):
+def new_user():
     user_dict = {}
     user_input = input('Insert a name: ')
     user_pwd = input('Insert a password: ')
     user_dict['name'] = user_input
     user_dict['pwd'] = user_pwd
-    db.append(user_dict)
-    return db
+    return user_dict
 
-def write_json(db):
+
+def write_json(dic):
     with open(f'{CWD}/users.json', 'w', encoding = 'utf8') as file:
-        json.dump({'users' : db}, file, ensure_ascii = False, indent = 4)
+        json.dump({'users' : [dic]}, file, ensure_ascii = False, indent = 4)
 
 def open_json():
-    with open(f'{CWD}/users.json', encoding = 'utf8') as file:
+    with open(f'{CWD}/users.json', 'r+', encoding = 'utf8') as file:
         data = json.load(file)['users']
+        file.seek(0)
     return data
 
-# def upd_json(db):
-#     with open(f'{CWD}/users.json', 'a', encoding = 'utf8') as file:
-#         json.dump(db, file['users'], ensure_ascii = False, indent = 4)
+def append_json(new_data):
+    with open(f'{CWD}/users.json', 'r+', encoding = 'utf8') as file:
+        data = json.load(file)
+        data["users"].append(new_data)
+        file.seek(0)
+        json.dump(data, file, ensure_ascii = False, indent = 4)
+
+
+def submenu_end(count):  
+        user = input('Where do you want to go now?\nPlease, insert "e" for exit or "m" for menu: ')
+        if user.lower() == 'e':
+            count = 1
+            return count
+        elif user.lower() == 'm':
+            count = 0
+            return count
+        else:
+            print('Invalid input. Returning back to the main menu...')
+            count = 0
+            return count
