63a24559e35f0b4dc5b2c39980fd2fd7a99d99c5
Roberto Vecino Alonso <85903780+Robvecino@users.noreply.github.com>
2021-07-23 22:03:42 +0200

    Add files via upload

--- funcs.py
+++ funcs.py
@@ -1,15 +1,72 @@
 
+
+# import requests as req
 import json
 import os
 import datetime
 import bcrypt
 
+
 CWD = os.path.dirname(__file__)
+
+# url = 'https://datos.comunidad.madrid/catalogo/dataset/35609dd5-9430-4d2e-8198-3eeb277e5282/resource/c38446ec-ace1-4d22-942f-5cc4979d19ed/download/desfibriladores_externos_fuera_ambito_sanitario.json'
+
+# res = req.get(url).json()
+
 def menu():
-    print('1. Create user')
+    print('Welcome to DEA APP'.center(50, '-'))
+    print('1. Users')
+    print('2. Enter to DEA')
+    print('3. Exit')
+
+def submenu():
+    print('1. Create a new user')
     print('2. Login')
-    print('3. Change password')
-    print('4. Exit')
+    print('3. Back to main menu')
+
+def submenu2():
+    print('1. Search DEA by code')
+    print('2. Search nearest DEA by location')
+    print('3. Modify DEA by code')
+    print('4. Back to main menu')
+
+# def write_json():
+#     with open(f'{CWD}/dea.json', 'w', encoding = 'utf8') as file:
+#         json.dump(res, file, ensure_ascii = False, indent = 4)
+
+
+def open_json():
+    with open(f'{CWD}/dea.json', encoding = 'utf8') as file:
+        data = json.load(file)['data']
+    return data
+
+def open_json_user():
+    with open(f'{CWD}/users.json', encoding = 'utf8') as file:
+        data = json.load(file)['users']
+    return data
+    
+
+# data = open_json()
+# print(len(data))
+
+# list_cp = ['28029', '28036', '28046', '28039', '28016', '28020', '28002', '28003', '28015', '28010', '28006', '28028', '28008', '28004', '28001', '280013', '28014', '28009', '28007', '28012', '28005', '28045']
+
+# count = 0
+
+# for dea in data:
+#     if dea['direccion_codigo_postal'] in list_cp:
+#         count += 1
+
+# print(count)
+
+# search2 = list(filter(lambda dea: dea['tipo_titularidad'] == 'Pública', data))
+# search3 = list(filter(lambda dea: dea['tipo_titularidad'] == 'Privada', data))
+# print(len(search2))
+# print(len(search3))
+
+def write_json_users(dic):
+    with open(f'{CWD}/users.json', 'w', encoding = 'utf8') as file:
+        json.dump({'users' : [dic]}, file, ensure_ascii = False, indent = 4)
 
 def new_user():
     user_dict = {}
@@ -20,32 +77,8 @@ def new_user():
     user_dict['name'] = user_input
     user_dict['pwd'] = final_pwd
     user_dict['user_since'] = datetime.date.today().isoformat()
+    print('New user successfully created!')
     return user_dict
-
-def change_pwd(db):
-    user_input = input('Insert your name: ')
-    user_pwd1 = input('Insert a new password: ').encode()
-    user_pwd2 = input('Insert the new password again: ').encode()
-    if user_pwd1 == user_pwd2:
-        password_hash = bcrypt.hashpw(user_pwd2, bcrypt.gensalt())
-        final_pwd = password_hash.decode()
-        for client in db:
-            if user_input == client['name']:
-                client['pwd'] == final_pwd
-                print('Your password has been successfully changed!')
-            else:
-                print('You are not registred')
-    else:
-        print('The new passwords did not match')
-
-def write_json(dic):
-    with open(f'{CWD}/users.json', 'w', encoding = 'utf8') as file:
-        json.dump({'users' : [dic]}, file, ensure_ascii = False, indent = 4)
-
-def open_json():
-    with open(f'{CWD}/users.json', encoding = 'utf8') as file:
-        data = json.load(file)['users']
-    return data
 
 def append_json(new_data):
     with open(f'{CWD}/users.json', 'r+', encoding = 'utf8') as file:
@@ -66,6 +99,19 @@ def login(db):
     else:
         print("Invalid entry! Access denied")
 
+def search_by_code(user_input, db):
+    for dea in db:
+        if dea['codigo_dea'] == user_input:
+            print(dea)
+
+def search_by_loc(db, h_user):
+    result = []
+    for dea in db:
+        h = ((int(dea['direccion_coordenada_x']) ** 2) + (int(dea['direccion_coordenada_y']) ** 2)) * 0.5
+        result.append(h)
+    result2 = sorted(result, key = lambda h: h > h_user)
+    return result2[0]
+
 def submenu_end(count):  
         user = input('Where do you want to go now?\nPlease, insert "e" for exit or "m" for menu: ')
         if user.lower() == 'e':
@@ -78,3 +124,20 @@ def submenu_end(count):
             print('Invalid input. Returning back to the main menu...')
             count = 0
             return count
+
+class Dea():
+    def __init__(self, x ,y):
+        self.x = x
+        self.y = y
+
+    @property
+    def search_by_loc(self):
+        h = ((self.x ** 2) + (self.y ** 2)) * 0.5
+        return h
+
+des = Dea(325, 456)
+result = des.search_by_loc
+print(result)
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+
 import json
-from json.decoder import JSONDecodeError
 import funcs
 import datetime
 
@@ -11,32 +11,64 @@ while count < 1:
     funcs.menu()
     user = input('Choose an option: ')
     if user == '1':
-        try:
-            data = funcs.open_json()
-            new_user = funcs.new_user()
-            for user in data:
-                if new_user['name'] == user['name']:
-                    new_user['name'] = input('This name has already been used. Please, select another: ')
-            funcs.append_json(new_user)
-        except JSONDecodeError:
-            new_user = funcs.new_user()
-            funcs.write_json(new_user)
-        count = funcs.submenu_end(count)
-    elif user == '2':
-        data = funcs.open_json()
-        user_token = funcs.login(data)
-        server_token = user_token
-        count = funcs.submenu_end(count)
-    elif user == '3':
-        data = funcs.open_json()
-        if user_token:
-            if (user_token['expire_date']  + datetime.timedelta(minutes = 1)) > datetime.datetime.today():
-                if user_token == server_token:
-                    funcs.change_pwd(data)
-            else:
-                print('Your session has already expired. Please, login again')
+        smcount = 0
+        funcs.submenu()
+        user = input('Choose an option: ')
+        if user == '1':
+            try:
+                data = funcs.open_json_user()
+                new_user = funcs.new_user()
+                for user in data:
+                    if new_user['name'] == user['name']:
+                        new_user['name'] = input('This name has already been used. Please, select another: ')
+                funcs.append_json(new_user)
+            except FileNotFoundError:
+                new_user = funcs.new_user()
+                funcs.write_json_users(new_user)
+            smcount = funcs.submenu_end(count)
+        elif user == '2':
+            data = funcs.open_json_user()
+            user_token = funcs.login(data)
+            server_token = user_token
+            smcount = funcs.submenu_end(count)
+        elif user == '3':
+            smcount += 1
         else:
-            print('You are not loged. Please, login to access.')
-        count = funcs.submenu_end(count)
-    elif user == '4':
-        count += 1
+            print('Invalid input!')
+            smcount = 0
+    elif user == '2':
+        sm2count = 0
+        funcs.submenu2()
+        user = input('Choose an option: ')
+        if user == '1':
+            data = funcs.open_json()
+            user = input('Insert a DEA code to search: ')
+            funcs.search_by_code(user, data)
+            sm2count = funcs.submenu_end(count)
+        elif user == '2':
+            data = funcs.open_json()
+            x_user = int(input('Insert your X location: '))
+            y_user = int(input('Insert your Y location: '))
+            h_user = Dea(x_user, y_user)
+            user_result = h_user.search_by_loc
+            h_data = funcs.search_by_loc(data)
+
+            sm2count = funcs.submenu_end(count)
+                        
+        elif user == '3':
+            data = funcs.open_json()
+            # if user_token:
+            #     if (user_token['expire_date']  + datetime.timedelta(minutes = 30)) > datetime.datetime.today():
+            #         if user_token == server_token:
+
+            #     else:
+            #         print('Your session has already expired. Please, login again')
+            # else:
+            #     print('You are not loged. Please, login to access.')
+            sm2count = funcs.submenu_end(count)
+        elif user == '4':
+            sm2count += 1
+        else:
+            print('Invalid input!')
+            smcount = 0
+
