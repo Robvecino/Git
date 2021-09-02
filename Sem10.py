542a58dd6f66c99bcc80defefeb6b6b40aa7e294
Roberto Vecino Alonso <85903780+Robvecino@users.noreply.github.com>
2021-09-02 21:57:08 +0200

    Update Sem10.py

--- Sem10.py
+++ Sem10.py
@@ -92,3 +92,50 @@ print(user_test.salary)
 
 #? Primero, se crean pruebas unitarias para ver si funciona el código para, después, implementar el código que se ha probado en el test. Para ello, se utiliza la librería «unittest». La explicación continúa en la carpeta «testing».
 
+#! Clase jueves 02/09
+
+#* Funciones generadoras
+
+a = [1, 2, 3, 4]
+
+#* A continuación, se muestran formas de realizar iterators de forma común:
+
+counter = 0
+while counter < len(a):
+    print(a[counter])
+    counter += 1
+
+for element in a:
+    print(element)
+
+#? Los iterators, en las funciones generadoras, conocen cuáles son las posiciones de los iterables. Las funciones «filter» y «map» son los iterators por antonomasia.
+
+b = map(lambda num: num ** 2, a)
+
+print(next(b)) #TODO En los iterators siempre se pueden utilizar la función «next». Si no, es que no es un iterator.
+print(next(b))
+print(next(b))
+
+def dobles(dataset):
+    result = []
+    for num in dataset:
+        result.append(num ** 2)
+    return result
+
+c = dobles(a)
+
+def gen_dobles(dataset): #? Esta es una función generadora de la función anterior. Devuelve un «generator object» y no ocupa espacio en memoria
+    for num in dataset:
+        yield num ** 2
+
+d = gen_dobles(a)
+
+#* No hace falta guardar el resultado en una variable:
+
+for num in gen_dobles(a):
+    print(num)
+
+#* Se puede realizar funciones generadoras en formato de compresión de lista:
+
+e = (num ** 2 for num in a)
+print(e)
